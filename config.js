0f4bbaca14f04165353ed30ed4ffa063f6de1627
hui702 <saviovivian70@gmail.com>
2026-09-23 22:20:05 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 window.INVITE_CONFIG = {
-  pageTitle: "正式邀請妳",
-  badgeText: "💌 正式邀請函",
-  mainTitle: "可惡！我正想開始做一個，你就傳 IG 給我了 😤❤️",
-  inviteMessage: `既然都被妳破梗了，那我還是要正式邀請妳～
-10/30（五）或是 10/31（六）可以撥點時間給我嗎？
-想帶妳一起出去走走、散散心 🚗✨`,
-  sceneText: "行程準備中：可愛、舒服、沒有壓力，只有一點點期待。",
+  pageTitle: "被你破梗了拉!!",
+  badgeText: "💌 想要邀請你跟我出去玩",
+  mainTitle: "欸！我正在做的時候，你就傳 IG 給我了😤",
+  inviteMessage: `既然都被妳破梗了，我還是要做一個給你～
+10/30（五）或是 10/31（六）可以留點時間給我嗎？
+想跟妳一起出去走走、散散心 因為你接下來要上班了 壓力一定很大吧!`,
+  sceneText: "行程準備中：舒服、坐在咖啡廳放鬆也很可以",
   yesButtonText: "沒問題！❤️",
   noButtonText: "不要 😜",
   noButtonTeaseText: "真的不要？😗",
   successTitle: "耶！約定好了喔！",
-  successMessage: "我開始來安排行程，到時候見 🥰",
+  successMessage: "我來規畫一下行程，到時候見 🥰",
   lineButtonText: "一鍵用 LINE 傳給你",
   hintText: "如果 LINE 沒有自動打開，可以點上面的按鈕送出預填訊息。",
   lineMessage: "我答應你的約會啦！10/30 或 10/31 見！🥰",
@@ -29,11 +29,11 @@ window.INVITE_CONFIG = {
       },
       10: {
         title: "拜託啦～",
-        message: "真的不考慮一下嗎？拜託啦，就給我一點點時間嘛 🥹❤️"
+        message: "真的不考慮一下嗎？拜託啦，就給我一點點時間嘛🥲"
       },
       12: {
         title: "不給按了！",
-        message: "你按太多次了，不給你按了 😤\n現在只剩下一個選項可以選喔 ❤️",
+        message: "你按太多次了，不給你按了 😤\n現在只剩下一個選項可以選了😍",
         removeNoButton: true
       }
     }
