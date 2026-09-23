b5e4b04ab9510f559a8fa82adba16f0d441a25e9
hui702 <saviovivian70@gmail.com>
2026-09-24 00:06:39 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -3,12 +3,14 @@ window.INVITE_CONFIG = {
   badgeText: "💌 想要邀請你跟我出去玩",
   mainTitle: "欸！我正在做的時候，你就傳 IG 給我了😤",
   inviteMessage: `既然都被妳破梗了，我還是要做一個給你～
-9/28 或是 10/30 可以留點時間給我嗎？
+09/30 或是 10/07 可以留點時間給我嗎？
 想跟妳一起出去走走、散散心 因為你接下來要上班了 壓力一定很大吧!`,
   sceneText: "行程準備中：舒服、坐在咖啡廳放鬆也很可以",
   yesButtonText: "沒問題！❤️",
   noButtonText: "不要 😜",
   noButtonTeaseText: "真的不要？😗",
+  successNoteBadge: "想對你說的話",
+  successNoteText: "雖然已經被你破梗了，但你傳給我的時候我做一半了，除了希望你收到會開心會喜歡之外，我知道我很理性、我很直，但也是真的希望你以後上班有不開心，或者有壓力的時候都可以跟我說，我也想成為分擔你情緒的那個人，這個日子我們就一起出去晃晃走走散散心吧!\n\n希望你可以一直都開開心心的，要沒壓力是不可能的，但我會一直都在你身邊陪著你哦😙",
   successTitle: "耶！約定好了喔！",
   successMessage: "我來規畫一下行程，到時候見 🥰",
   discordMessageTemplate: "有人答應你的約會啦！她選：{date} 🥰",
@@ -18,9 +20,9 @@ window.INVITE_CONFIG = {
   dateModalTitle: "那妳想選哪一天呢？",
   dateModalMessage: "選一個時間，我就開始安排舒服的小行程。",
   dateOptions: [
-    { label: "9/28", value: "9/28" },
-    { label: "10/30", value: "10/30" },
-    { label: "兩天都可以", value: "9/28 和 10/30 都可以" }
+    { label: "09/30", value: "09/30" },
+    { label: "10/07", value: "10/07" },
+    { label: "兩天都可以", value: "09/30 和 10/07 都可以" }
   ],
   celebrationIcons: ["❤️", "💕", "✨", "💗", "🌸", "🥰"],
   runaway: {
